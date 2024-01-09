098607038b01b59a13eece5d56921dffbce55f39
Koushik Das <gentlekoushikdas@gmail.com>
2024-01-09 23:13:46 +0530

    1st commit

---
+++ Mavenproject2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package whatever.mavenproject2;
+
+/**
+ *
+ * @author KOUSHIK DAS
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        int a,b,c;
+        a=10;
+        b=20;
+        c=a+b;
+        System.out.println("Result is "+ c);
+    }
+}
