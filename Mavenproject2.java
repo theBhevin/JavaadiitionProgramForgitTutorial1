67b11f4edccf4063c9460be67b753bfedcf0e28e
Koushik Das <gentlekoushikdas@gmail.com>
2024-01-09 23:42:38 +0530

    2nd Commit

--- Mavenproject2.java
+++ Mavenproject2.java
@@ -3,7 +3,7 @@
  */
 
 package whatever.mavenproject2;
-
+import java.util.Scanner;
 /**
  *
  * @author KOUSHIK DAS
@@ -12,9 +12,19 @@ public class Mavenproject2 {
 
     public static void main(String[] args) {
         int a,b,c;
-        a=10;
+     /*   a=10;
         b=20;
         c=a+b;
         System.out.println("Result is "+ c);
+*/
+     Scanner xc = new Scanner(System.in);
+        System.out.println("Enter 1st number");
+     a=xc.nextInt();
+     System.out.println("Enter 2nd number");
+     b=xc.nextInt();
+     c=a+b;
+        System.out.println("Result is "+c);
+
+    
     }
 }
